07b0d37d5e5bf7a7241a3a821525d72082400939
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-22 23:22:31 -0400

    Style

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // OAUTH 2.0 Client ID
 const CLIENT_ID =
     '196395003919-thc20nqbuukhnmsm1v12o7snh2od7jco.apps.googleusercontent.com';
@@ -53,3 +67,4 @@ function renderButton() {
     'onfailure': () => {}
   });
 }
+
