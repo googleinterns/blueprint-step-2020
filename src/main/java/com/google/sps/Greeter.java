f1594f01f423d1ca0d681b25fb34cdf24b2b977f
Guilherme Andrade <guiandrade@google.com>
2020-06-24 01:02:34 -0400

    Fix linter issues

--- src/main/java/com/google/sps/Greeter.java
+++ src/main/java/com/google/sps/Greeter.java
@@ -17,9 +17,6 @@ package com.google.sps;
 /** Utility class for creating greeting messages. */
 public class Greeter {
 
-
-
-    
   /** Returns a greeting for the given name. */
   public String greet(String name) {
     return "Hello " + name;
