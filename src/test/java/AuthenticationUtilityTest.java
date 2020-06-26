0e6a54c519d0282949c9d7591a910bfda1008cd3
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-26 10:43:14 -0400

    Some Style Changes

--- src/test/java/AuthenticationUtilityTest.java
+++ src/test/java/AuthenticationUtilityTest.java
@@ -1,15 +1,32 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import com.google.sps.utility.AuthenticationUtility;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
 import org.mockito.Mockito;
 
 /**
- * Class to Test the Authentication Utility functions TODO: Find a way to mock the Google
- * verification service to create unit tests for AuthenticationUtility.verifyUserToken()
+ * Class to Test the Authentication Utility functions
+ * TODO: Find a way to mock the Google verification service (Issue #5)
  */
 @RunWith(JUnit4.class)
 public final class AuthenticationUtilityTest {
