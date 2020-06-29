0d7e9911a2d8fb2a7ca0f76d65154e4e6d12df57
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-29 13:20:51 -0400

    Rename cookie value in test

--- src/test/java/AuthenticationUtilityTest.java
+++ src/test/java/AuthenticationUtilityTest.java
@@ -30,14 +30,14 @@ public final class AuthenticationUtilityTest {
 
   private final Cookie[] correctCookies =
       new Cookie[] {
-        new Cookie("junk", "junk_Value"),
+        new Cookie("junk", "junk_value"),
         new Cookie("idToken", "sample_id_token"),
         new Cookie("accessToken", "sample_access_token")
       };
 
   private final Cookie[] emptyCookies = new Cookie[] {};
 
-  private final Cookie[] missingAuthCookies = new Cookie[] {new Cookie("junk", "junk_Value")};
+  private final Cookie[] missingAuthCookies = new Cookie[] {new Cookie("junk", "junk_value")};
 
   private final Cookie[] duplicateCookies =
       new Cookie[] {
