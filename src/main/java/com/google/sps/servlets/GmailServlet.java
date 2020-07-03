5eda7a2df9a1ded58f42a721ed5d38bcc4659214
Austin Teshuba <austinteshuba19@gmail.com>
2020-07-03 15:21:48 -0400

    Create simple servlet

---
+++ src/main/java/com/google/sps/servlets/GmailServlet.java
@@ -0,0 +1,37 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.services.gmail.Gmail;
+import com.google.sps.utility.AuthenticationUtility;
+import com.google.sps.utility.GmailUtility;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/** Serves the OAuth 2.0 Client ID */
+@WebServlet("/gmail")
+public class GmailServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    Credential googleCredential = AuthenticationUtility.getGoogleCredential(request);
+
+  }
+}
