125507a7f0d8f5642147ddf612a5447100a9f64f
Austin Teshuba <austinteshuba19@gmail.com>
2020-07-03 12:28:26 -0400

    Add Test File and fix style

---
+++ src/test/java/GmailUtilityTest.java
@@ -0,0 +1,6 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Test Gmail Utility functions TODO: Mock the Gmail Service features (Issue #) */
+@RunWith(JUnit4.class)
+public final class GmailUtilityTest {}
