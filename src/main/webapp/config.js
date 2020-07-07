7610c12924b43097edbe1021eb3ff1b14209d931
Zachary Tan <tanzachary@google.com>
2020-07-07 19:42:27 -0400

    Debug

--- src/main/webapp/config.js
+++ src/main/webapp/config.js
@@ -1,6 +1,6 @@
 // Import all env vars from .env file
 require('dotenv').config()
 
-export const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY
 
 console.log(API_KEY) // => Hello
