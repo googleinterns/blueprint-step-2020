383abdd29cdc27e8b6693b675bb8c4ee9593ba46
Zachary Tan <tanzachary@google.com>
2020-07-07 15:44:07 -0400

    Add javascript function to display api key

--- src/main/java/com/google/sps/servlets/SecretManagerServlet.java
+++ src/main/java/com/google/sps/servlets/SecretManagerServlet.java
@@ -1,17 +1,25 @@
-package com.google.sps.servlets;	
+package com.google.sps.servlets;
 
-import java.io.IOException;	
-import java.io.PrintWriter;	
-import javax.servlet.annotation.WebServlet;	
-import javax.servlet.http.HttpServlet;	
-import javax.servlet.http.HttpServletRequest;	
-import javax.servlet.http.HttpServletResponse;	
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
 
 @WebServlet("/secret-manager")	
 public class SecretManagerServlet extends HttpServlet {	
+
   @Override	
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {	
     String API_KEY = System.getProperty("API_KEY");
-    request.setAttribute("API_KEY", API_KEY);
-  }	
+    sendJson(response, API_KEY);
+  }
+
+  public static void sendJson(HttpServletResponse response, Object object) throws IOException {
+    Gson gson = new Gson();
+    String json = gson.toJson(object);
+    response.getWriter().println(json);
+  }
 }
---
+++ src/main/webapp/apikey.js
@@ -0,0 +1,10 @@
+function getApiKey() {
+  fetch('/secret-manager')
+    .then((response) => (response.json()))
+    .then((apikey) => {
+      console.log(apikey);
+      document.getElementById('api-key').innerHTML = apikey;
+    });
+}
+
+
