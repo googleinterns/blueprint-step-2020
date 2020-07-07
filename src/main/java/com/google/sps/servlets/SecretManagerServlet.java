e63537ba37274191879b939403d414747e0d1cff
Zachary Tan <tanzachary@google.com>
2020-07-07 14:33:53 -0400

    Fix formatting issues

--- src/main/java/com/google/sps/servlets/SecretManagerServlet.java
+++ src/main/java/com/google/sps/servlets/SecretManagerServlet.java
@@ -1,9 +1,7 @@
-
 package com.google.sps.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
