0407de8f3dadd4d3482737dbfcbb2c5bb9e0297f
Zachary Tan <tanzachary@google.com>
2020-07-07 18:40:13 -0400

    Debug error

--- src/main/java/com/google/sps/servlets/SecretManagerServlet.java
+++ src/main/java/com/google/sps/servlets/SecretManagerServlet.java
@@ -13,7 +13,7 @@ public class SecretManagerServlet extends HttpServlet {
 
   @Override	
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {	
-    String apiKey = System.getenv("apikey");
+    String apiKey = System.getenv("API_KEY");
     sendJson(response, apiKey);
   }
 
