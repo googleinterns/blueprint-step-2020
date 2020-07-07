eb810449f0118807995703abf45b70c6e25c9ec8
Zachary Tan <tanzachary@google.com>
2020-07-06 20:36:54 -0400

    Add feature to find Google Application Credentials automatically

--- src/main/java/com/google/sps/utility/SecretUtility.java
+++ src/main/java/com/google/sps/utility/SecretUtility.java
@@ -4,8 +4,25 @@ import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.cloud.secretmanager.v1.SecretVersionName;
 import java.io.IOException;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 public class SecretUtility {
+
+  private static void authImplicit() {
+    // If you don't specify credentials when constructing the client, the client library will
+    // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+    Storage storage = StorageOptions.getDefaultInstance().getService();
+  
+    System.out.println("Buckets:");
+    Page<Bucket> buckets = storage.list();
+    for (Bucket bucket : buckets.iterateAll()) {
+      System.out.println(bucket.toString());
+    }
+  }
+
   public static String accessSecretVersion() throws IOException {
     String projectId = "step7-2020";
     String secretId = "API_KEY";
@@ -21,6 +38,7 @@ public class SecretUtility {
     // once, and can be reused for multiple requests. After completing all of your requests, call
     // the "close" method on the client to safely clean up any remaining background resources.
     try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+      authImplicit();
       SecretVersionName secretVersionName = SecretVersionName.of(projectId, secretId, versionId);
 
       // Access the secret version.
