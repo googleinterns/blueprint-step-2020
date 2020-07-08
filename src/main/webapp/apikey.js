d014e8cafbd6c7969c90b287b72c62e78bd16d32
Zachary Tan <tanzachary@google.com>
2020-07-08 11:29:08 -0400

    Debug

--- src/main/webapp/apikey.js
+++ src/main/webapp/apikey.js
@@ -1,7 +1,8 @@
 function getApiKey() {
   fetch('/secret-manager')
-    .then((response) => {
-      console.log(response);
-      document.getElementById('api-key').innerHTML = response;
+    .then((response) => (response.json()))
+    .then((apikey) => {
+      console.log(apikey);
+      document.getElementById('api-key').innerHTML = apikey;
     });
 }
