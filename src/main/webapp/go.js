894080c4c2cf3bf10ddaea036fc6015bf69b1c53
Zachary Tan <tanzachary@google.com>
2020-07-08 13:24:38 -0400

    Ignore warning

--- src/main/webapp/go.js
+++ src/main/webapp/go.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* exported populateGo */
+
 /**
  * Gets key information from each leg and displays them on the DOM.
  */
