3ae0c4260cd6281ab7aa0436a50dd72c57000f13
Zachary Tan <tanzachary@google.com>
2020-07-12 23:19:20 -0400

    Add javadoc comments to public methods

--- src/main/java/com/google/maps/DirectionsApplication.java
+++ src/main/java/com/google/maps/DirectionsApplication.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.maps;
 
 import java.net.URI;
@@ -5,14 +19,20 @@ import java.net.URISyntaxException;
 import java.util.List;
 import org.apache.http.client.utils.URIBuilder;
 
+/**
+ * Application class which fulfills the declared Directions consumer contract.
+ */
 public class DirectionsApplication implements DirectionsConsumer {
   private DirectionsService directionsService;
 
+  /**
+   * Changes constructor to require an instantiated service.
+   */
   public DirectionsApplication(DirectionsService service) {
     this.directionsService = service;
   }
 
-  private static String buildWaypoints(List<String> waypoints) {
+  private String buildWaypoints(List<String> waypoints) {
     StringBuilder waypointsBuilder = new StringBuilder("optimize:true");
     if (waypoints.isEmpty()) {
       return waypointsBuilder.toString();
@@ -23,7 +43,7 @@ public class DirectionsApplication implements DirectionsConsumer {
     return waypointsBuilder.toString();
   }
 
-  public static String getDirectionsUri(
+  private String getDirectionsUri(
       String destination, String origin, List<String> waypoints, String apiKey)
       throws URISyntaxException {
     URI uri =
--- src/test/java/DirectionsTest.java
+++ src/test/java/DirectionsTest.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import com.google.maps.DirectionsApplication;
 import com.google.maps.DirectionsConsumer;
 import com.google.maps.DirectionsService;
@@ -17,15 +31,12 @@ public class DirectionsTest {
   private DirectionsServiceInjector injector;
   private final String ORIGIN = "University of Waterloo";
   private final String DESTINATION = "Wilfrid Laurier University";
-  // private final List<String> NO_WAYPOINTS = Arrays.asList();
   private final List<String> ONE_WAYPOINT = Arrays.asList("University of Waterloo Place");
-  // private final List<String> MANY_WAYPOINTS =
-  //    Arrays.asList("University of Waterloo Place", "Google Kitchener");
   private final String API_KEY = "SAMPLE_API_KEY";
 
   @Before
   public void init() {
-    // Mock the injector with anonymous class
+    // Mock the injector with an anonymous class
     injector =
         new DirectionsServiceInjector() {
           @Override
@@ -49,7 +60,9 @@ public class DirectionsTest {
   }
 
   @Test
-  public void test() {
+  public void getDirections() {
+    // Destination and origin are not the same, only one waypoint.
+    // null is expected since Directions service is mocked.
     DirectionsConsumer consumer = injector.getConsumer();
     String result = consumer.processDirections(DESTINATION, ORIGIN, ONE_WAYPOINT, API_KEY);
     Assert.assertNull(result);
