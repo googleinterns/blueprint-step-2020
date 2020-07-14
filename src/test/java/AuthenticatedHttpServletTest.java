78150f6c872ec6f002cf8bd6ff7fd6f5baaba04a
Austin Teshuba <austinteshuba19@gmail.com>
2020-07-14 14:40:49 -0400

    Lint

--- src/test/java/AuthenticatedHttpServletTest.java
+++ src/test/java/AuthenticatedHttpServletTest.java
@@ -28,10 +28,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 
-
-/**
- * Tests the AuthenticatedHttpServlet abstract class
- */
+/** Tests the AuthenticatedHttpServlet abstract class */
 @RunWith(JUnit4.class)
 public class AuthenticatedHttpServletTest {
   // Mock abstract servlet with real constructor and real methods
