df031faf9c248c8b4feeadee3d95d88aa8f4b235
Zachary Tan <tanzachary@google.com>
2020-07-15 23:11:25 -0400

    Rewrite tests to check decoded values

--- src/test/java/JsonUtilityTest.java
+++ src/test/java/JsonUtilityTest.java
@@ -12,94 +12,94 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.google.sps.utility.JsonUtility;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Type;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
 
 /** Test Json Utility methods */
 @RunWith(JUnit4.class)
 public class JsonUtilityTest {
 
-  private final String STRING_OBJECT = "HELLO WORLD";
-  private final List<Integer> LIST_OBJECT = Arrays.asList(1, 3, 5, 9, 7);
-  private final Map<String, Integer> MAP_OBJECT =
-      new HashMap<String, Integer>() {
-        {
-          put("a", 1);
-          put("c", 3);
-        }
-      };
+  private static final String STRING_OBJECT = "HELLO WORLD";
+  private static final List<Integer> LIST_OBJECT = Arrays.asList(1, 3, 5, 9, 7);
+  private static final Map<String, Integer> MAP_OBJECT =
+      ImmutableMap.of(
+          "One", Integer.valueOf(1), "Two", Integer.valueOf(2), "Three", Integer.valueOf(3));
 
-  private static HttpServletResponse response = mock(HttpServletResponse.class);
-  private static StringWriter stringWriter = new StringWriter();
-  private static PrintWriter printWriter = new PrintWriter(stringWriter);
+  private static HttpServletResponse response;
+  private static StringWriter stringWriter;
+  private static PrintWriter printWriter;
   private final Gson gson = new Gson();
 
-  @BeforeClass
-  public static void init() throws IOException {
-    // Dependency injection before tests
-    when(response.getWriter()).thenReturn(printWriter);
-  }
-
   @Before
-  public void flush() {
-    // Flush in case text is still in buffer
-    stringWriter.flush();
+  public void init() throws IOException {
+    // Mock servlet response writer
+    response = Mockito.mock(HttpServletResponse.class);
+    stringWriter = new StringWriter();
+    printWriter = new PrintWriter(stringWriter);
+    Mockito.when(response.getWriter()).thenReturn(printWriter);
   }
 
   @After
-  public void clear() {
+  public void clear() throws IOException {
     // Dump contents after each test
     stringWriter.getBuffer().setLength(0);
+    stringWriter.close();
+    printWriter.close();
   }
 
   @Test
   public void sendStringToResponse() throws IOException {
-    // String should be sent successfully and in the expected format
+    // String should be sent successfully and have the same contents as the sent object.
     JsonUtility.sendJson(response, STRING_OBJECT);
+    printWriter.flush();
 
-    String expected = gson.toJson(STRING_OBJECT);
-    String actual = stringWriter.toString();
+    String actual = gson.fromJson(stringWriter.toString(), String.class);
 
-    Assert.assertTrue(actual.contains(expected));
+    Assert.assertEquals(STRING_OBJECT, actual);
   }
 
   @Test
   public void sendListToResponse() throws IOException {
-    // List should be sent successfully and in the expected format
+    // List should be sent successfully and have the same contents as the sent object.
     JsonUtility.sendJson(response, LIST_OBJECT);
+    printWriter.flush();
 
-    String expected = gson.toJson(LIST_OBJECT);
-    String actual = stringWriter.toString();
+    String actualString = stringWriter.toString();
+    Type type = new TypeToken<List<Integer>>() {}.getType();
+    List<Integer> actual = gson.fromJson(actualString, type);
 
-    Assert.assertTrue(actual.contains(expected));
+    Assert.assertEquals(LIST_OBJECT, actual);
   }
 
   @Test
   public void sendMapToResponse() throws IOException {
-    // Map should be sent successfully and in the expected format
+    // Map should be sent successfully and have the same contents as the sent object.
     JsonUtility.sendJson(response, MAP_OBJECT);
+    printWriter.flush();
 
-    String expected = gson.toJson(MAP_OBJECT);
-    String actual = stringWriter.toString();
+    String actualString = stringWriter.toString();
+    Type type = new TypeToken<Map<String, Integer>>() {}.getType();
+    Map<String, Integer> actual = gson.fromJson(actualString, type);
 
-    Assert.assertTrue(actual.contains(expected));
+    Assert.assertEquals(MAP_OBJECT.get("One"), actual.get("One"));
+    Assert.assertEquals(MAP_OBJECT.get("Two"), actual.get("Two"));
+    Assert.assertEquals(MAP_OBJECT.get("Three"), actual.get("Three"));
   }
 }
