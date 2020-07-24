3ea4088a818a810ee7912eb69ed50d07674cfc0b
Zachary Tan <tanzachary@google.com>
2020-07-24 19:29:15 -0400

    Create KEYS.json in ClientIDServletTest

--- src/test/java/ClientIDServletTest.java
+++ src/test/java/ClientIDServletTest.java
@@ -14,27 +14,42 @@
 
 import com.google.sps.model.AuthenticationVerifier;
 import com.google.sps.servlets.ClientIDServlet;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileWriter;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mockito;
 
 /** Test Client ID Servlet to ensure response contains a defined OAuth 2.0 client ID */
 @RunWith(JUnit4.class)
 public final class ClientIDServletTest extends ServletTestBase {
+
   private static final ClientIDServlet servlet = new ClientIDServlet();
+  private static final File file = new File("src/main/resources/KEYS.json");
+
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+    // Creates a new file in src/main/resources and writes clientId to the file.
+    file.getParentFile().mkdirs();
+    file.createNewFile();
+    FileWriter writer = new FileWriter(file);
+    writer.write("{\"clientId\" : \"sampleValue\"}");
+    writer.close();
+  }
+
+  @After
+  public void tearDown() {
+    // Removes the file created in src/main/resources from calling setUp.
+    file.delete();
+  }
 
   @Test
   public void responseContainsClientId() throws Exception {
     servlet.doGet(request, response);
-    System.out.println("PWD " + System.getProperty("user.dir"));
     Assert.assertTrue(stringWriter.toString().contains(AuthenticationVerifier.getClientId()));
   }
 }
