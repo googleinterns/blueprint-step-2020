17605217f9d13e8e0c86eb777faa5d8326b14f7b
Zachary Tan <tanzachary@google.com>
2020-07-30 10:51:53 -0400

    Stash

---
+++ src/main/java/com/google/sps/servlets/TasksSummaryServlet.java
@@ -0,0 +1,149 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.tasks.model.Task;
+import com.google.api.services.tasks.model.TaskList;
+import com.google.gson.Gson;
+import com.google.sps.model.AuthenticatedHttpServlet;
+import com.google.sps.model.AuthenticationVerifier;
+import com.google.sps.model.TasksClient;
+import com.google.sps.model.TasksClientFactory;
+import com.google.sps.model.TasksClientImpl;
+import com.google.sps.model.TasksResponse;
+import com.google.sps.utility.JsonUtility;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TasksSummaryServlet extends AuthenticatedHttpServlet {
+  private final TasksClientFactory tasksClientFactory;
+
+  /** Create servlet with default TasksClient and Authentication Verifier implementations */
+  public TasksSummaryServlet() {
+    tasksClientFactory = new TasksClientImpl.Factory();
+  }
+
+  /**
+   * Create servlet with explicit implementations of TasksClient and AuthenticationVerifier
+   *
+   * @param authenticationVerifier implementation of AuthenticationVerifier
+   * @param tasksClientFactory implementation of TasksClientFactory
+   */
+  public TasksSummaryServlet(
+      AuthenticationVerifier authenticationVerifier, TasksClientFactory tasksClientFactory) {
+    super(authenticationVerifier);
+    this.tasksClientFactory = tasksClientFactory;
+  }
+
+  /**
+   * Returns a summary of tasks from a user's Tasks account
+   *
+   * @param request Http request from client. Should contain idToken and accessToken
+   * @param response 403 if user is not authenticated, list of Tasks otherwise
+   * @param googleCredential a valid google credential object (already verified)
+   * @throws IOException if an issue arises while processing the request
+   */
+  @Override
+  public void doGet(
+      HttpServletRequest request, HttpServletResponse response, Credential googleCredential)
+      throws IOException {
+    assert googleCredential != null
+        : "Null credentials (i.e. unauthenticated requests) should already be handled";
+
+    // Get tasks from Google Tasks
+    TasksClient tasksClient = tasksClientFactory.getTasksClient(googleCredential);
+    List<TaskList> taskLists = tasksClient.listTaskLists();
+    List<Task> tasks = tasksClient.listAllTasksInAllTaskLists();
+
+    // Initialize Tasks Response
+    List<String> taskListTitles = getTaskListTitles(taskLists);
+    long tasksToCompleteCount = countTasksToComplete(tasks);
+    long tasksDueTodayCount = countTasksDueToday(tasks);
+    long tasksCompletedTodayCount = countTasksCompletedToday(tasks);
+    long tasksOverdueCount = countTasksOverdue(tasks);
+    TasksResponse tasksResponse =
+        TasksResponse.builder()
+            .taskListTitles(taskListTitles)
+            .tasksToCompleteCount(tasksToCompleteCount)
+            .tasksDueTodayCount(tasksDueTodayCount)
+            .tasksCompletedTodayCount(tasksCompletedTodayCount)
+            .tasksOverdueCount(tasksOverdueCount)
+            .build();
+
+    // Convert tasks to JSON and print to response
+    JsonUtility.sendJson(response, tasksResponse);
+  }
+
+  private List<String> getTaskListTitles(List<TaskList> taskLists) throws IOException {
+    return taskLists.stream().map(taskList -> taskList.getTitle()).collect(Collectors.toList());
+  }
+
+  private long countTasksToComplete(List<Task> tasks) {
+    // getHidden is defined for incomplete tasks
+    return tasks.stream().filter(task -> task.getHidden() == null).count();
+  }
+
+  private long countTasksDueToday(List<Task> tasks) {
+    String today = LocalDate.now().toString();
+    return tasks.stream()
+        .filter(task -> task.getDue() != null && task.getDue().contains(today))
+        .count();
+  }
+
+  private long countTasksCompletedToday(List<Task> tasks) {
+    ZoneId zoneId = ZoneId.systemDefault();
+    Instant startOfDay = LocalDate.now(zoneId).atStartOfDay(zoneId).toInstant();
+    Instant endOfDay = LocalDate.now(zoneId).plusDays(1).atStartOfDay(zoneId).toInstant();
+    // getHidden is defined for completed tasks
+    return tasks.stream()
+        .filter(task -> task.getHidden() != null)
+        .filter(
+            task -> {
+              Instant completionDateTime = ZonedDateTime.parse(task.getUpdated()).toInstant();
+              return completionDateTime.isAfter(startOfDay)
+                  && completionDateTime.isBefore(endOfDay);
+            })
+        .count();
+  }
+
+  private long countTasksOverdue(List<Task> tasks) {
+    ZoneId zoneId = ZoneId.systemDefault();
+    String zoneOffset = zoneId.getRules().getOffset(Instant.now()).toString();
+    Instant endOfDay = LocalDate.now(zoneId).plusDays(1).atStartOfDay(zoneId).toInstant();
+    return tasks.stream()
+        .filter(task -> task.getDue() != null)
+        .filter(
+            task -> {
+              DateTime dateTime = DateTime.parseRfc3339(task.getDue().replace("Z", zoneOffset));
+              long millis = dateTime.getValue();
+              Instant dueDate = new Date(millis).toInstant().plus(Period.ofDays(1));
+              return dueDate.isBefore(endOfDay);
+            })
+        .count();
+  }
+}
---
+++ src/test/java/TasksSummaryServletTest.java
@@ -0,0 +1,293 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import com.google.api.services.tasks.model.Task;
+import com.google.api.services.tasks.model.TaskList;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.common.collect.ImmutableList;
+import com.google.sps.model.AuthenticationVerifier;
+import com.google.sps.model.GmailResponse;
+import com.google.sps.model.TasksClient;
+import com.google.sps.model.TasksClientFactory;
+import com.google.sps.model.TasksResponse;
+import com.google.sps.servlets.TasksSummaryServlet;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.AdditionalAnswers;
+import org.mockito.Mockito;
+
+/**
+ * Test that the Tasks Servlet responds to the client with correct TasksResponse. Assumes
+ * AuthenticatedHttpServlet is functioning properly (those tests will fail otherwise).
+ */
+@RunWith(JUnit4.class)
+public final class TasksSummaryServletTest extends AuthenticatedServletTestBase {
+  private AuthenticationVerifier authenticationVerifier;
+  private TasksClientFactory tasksClientFactory;
+  private TasksClient tasksClient;
+  private TasksSummaryServlet servlet;
+  private HttpServletRequest request;
+  private HttpServletResponse response;
+  private StringWriter stringWriter;
+
+  private static final Gson gson = new Gson();
+
+  private static final String TASK_TITLE_ONE = "task one";
+  private static final String TASK_TITLE_TWO = "task two";
+  private static final String TASK_TITLE_THREE = "task three";
+  private static final String TASK_TITLE_FOUR = "task four";
+  private static final String TASK_TITLE_FIVE = "task five";
+  private static final String TASK_LIST_TITLE_ONE = "task list one";
+  private static final String TASK_LIST_TITLE_TWO = "task list two";
+
+  private static final TaskList TASK_LIST_ONE =
+      new TaskList().setTitle(TASK_LIST_TITLE_ONE).setId("taskListOne");
+  private static final TaskList TASK_LIST_TWO =
+      new TaskList().setTitle(TASK_LIST_TITLE_TWO).setId("taskListTwo");
+
+  private static final List<TaskList> NO_TASK_LISTS = ImmutableList.of();
+  private static final List<TaskList> ONE_TASK_LIST = ImmutableList.of(TASK_LIST_ONE);
+  private static final List<TaskList> TWO_TASK_LISTS =
+      ImmutableList.of(TASK_LIST_ONE, TASK_LIST_TWO);
+  private static final List<String> NO_TASK_LISTS_TITLES = ImmutableList.of();
+  private static final List<String> ONE_TASK_LIST_TITLES = ImmutableList.of(TASK_LIST_TITLE_ONE);
+  private static final List<String> TWO_TASK_LISTS_TITLES =
+      ImmutableList.of(TASK_LIST_TITLE_ONE, TASK_LIST_TITLE_TWO);
+
+  private static final DateTimeFormatter FORMATTER =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX").withZone(ZoneId.systemDefault());
+
+  private static final String YESTERDAY = FORMATTER.format(Instant.now().minus(1, ChronoUnit.DAYS));
+  private static final String TODAY = FORMATTER.format(Instant.now());
+  private static final String TOMORROW = FORMATTER.format(Instant.now().plus(1, ChronoUnit.DAYS));
+
+  private static final Task TASK_DUE_YESTERDAY =
+      new Task().setTitle(TASK_TITLE_TWO).setDue(YESTERDAY);
+  private static final Task TASK_DUE_TODAY = new Task().setTitle(TASK_TITLE_ONE).setDue(TODAY);
+  private static final Task TASK_DUE_TOMORROW =
+      new Task().setTitle(TASK_TITLE_THREE).setDue(TOMORROW);
+  private static final Task TASK_COMPLETED_YESTERDAY =
+      new Task().setTitle(TASK_TITLE_FOUR).setHidden(true).setUpdated(YESTERDAY);
+  private static final Task TASK_COMPLETED_TODAY =
+      new Task().setTitle(TASK_TITLE_FIVE).setHidden(true).setUpdated(TODAY);
+
+  private static final List<Task> NO_TASKS = ImmutableList.of();
+  private static final List<Task> TASKS_DUE_YESTERDAY = ImmutableList.of(TASK_DUE_YESTERDAY);
+  private static final List<Task> TASKS_DUE_TODAY = ImmutableList.of(TASK_DUE_TODAY);
+  private static final List<Task> TASKS_DUE_TOMORROW = ImmutableList.of(TASK_DUE_TOMORROW);
+  private static final List<Task> TASKS_COMPLETED_YESTERDAY =
+      ImmutableList.of(TASK_COMPLETED_YESTERDAY);
+  private static final List<Task> TASKS_COMPLETED_TODAY = ImmutableList.of(TASK_COMPLETED_TODAY);
+  private static final List<Task> ALL_TASKS =
+      ImmutableList.of(
+          TASK_DUE_YESTERDAY,
+          TASK_DUE_TODAY,
+          TASK_DUE_TOMORROW,
+          TASK_COMPLETED_YESTERDAY,
+          TASK_COMPLETED_TODAY);
+
+  private static final TasksResponse NO_TASK_LISTS_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(NO_TASK_LISTS_TITLES)
+          .tasksToCompleteCount(0)
+          .tasksDueTodayCount(0)
+          .tasksCompletedTodayCount(0)
+          .tasksOverdueCount(0)
+          .build();
+  private static final TasksResponse NO_TASKS_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(0)
+          .tasksDueTodayCount(0)
+          .tasksCompletedTodayCount(0)
+          .tasksOverdueCount(0)
+          .build();
+  private static final TasksResponse TASKS_DUE_YESTERDAY_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(1)
+          .tasksDueTodayCount(0)
+          .tasksCompletedTodayCount(0)
+          .tasksOverdueCount(1)
+          .build();
+  private static final TasksResponse TASKS_DUE_TODAY_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(1)
+          .tasksDueTodayCount(1)
+          .tasksCompletedTodayCount(0)
+          .tasksOverdueCount(0)
+          .build();
+  private static final TasksResponse TASKS_DUE_TOMORROW_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(1)
+          .tasksDueTodayCount(0)
+          .tasksCompletedTodayCount(0)
+          .tasksOverdueCount(0)
+          .build();
+  private static final TasksResponse TASKS_COMPLETED_YESTERDAY_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(0)
+          .tasksDueTodayCount(0)
+          .tasksCompletedTodayCount(0)
+          .tasksOverdueCount(0)
+          .build();
+  private static final TasksResponse TASKS_COMPLETED_TODAY_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(0)
+          .tasksDueTodayCount(0)
+          .tasksCompletedTodayCount(1)
+          .tasksOverdueCount(0)
+          .build();
+  private static final TasksResponse ALL_TASKS_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(ONE_TASK_LIST_TITLES)
+          .tasksToCompleteCount(3)
+          .tasksDueTodayCount(1)
+          .tasksCompletedTodayCount(1)
+          .tasksOverdueCount(1)
+          .build();
+  private static final TasksResponse MULTIPLE_TASK_LISTS_RESPONSE =
+      TasksResponse.builder()
+          .taskListTitles(TWO_TASK_LISTS_TITLES)
+          .tasksToCompleteCount(6)
+          .tasksDueTodayCount(2)
+          .tasksCompletedTodayCount(2)
+          .tasksOverdueCount(2)
+          .build();
+
+  @Override
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+
+    authenticationVerifier = Mockito.mock(AuthenticationVerifier.class);
+    tasksClientFactory = Mockito.mock(TasksClientFactory.class);
+    tasksClient = Mockito.mock(TasksClient.class);
+    servlet = new TasksSummaryServlet(authenticationVerifier, tasksClientFactory);
+
+    Mockito.when(tasksClientFactory.getTasksClient(Mockito.any())).thenReturn(tasksClient);
+    // Authentication will always pass
+    Mockito.when(authenticationVerifier.verifyUserToken(Mockito.anyString()))
+        .thenReturn(AUTHENTICATION_VERIFIED);
+
+    // Writer used in get/post requests to capture HTTP response values
+    stringWriter = new StringWriter();
+
+    request = Mockito.mock(HttpServletRequest.class);
+    response =
+        Mockito.mock(
+            HttpServletResponse.class,
+            AdditionalAnswers.delegatesTo(new HttpServletResponseFake(stringWriter)));
+
+    Mockito.when(request.getCookies()).thenReturn(validCookies);
+  }
+
+  @Test
+  public void noTaskLists() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(NO_TASK_LISTS);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(NO_TASK_LISTS_RESPONSE, actual);
+  }
+
+  @Test
+  public void noTasks() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(NO_TASKS);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(NO_TASKS_RESPONSE, actual);
+  }
+
+  @Test
+  public void taskOverdue() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(TASKS_DUE_YESTERDAY);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(TASKS_DUE_YESTERDAY_RESPONSE, actual);
+  }
+
+  @Test
+  public void taskDueToday() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(TASKS_DUE_TODAY);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(TASKS_DUE_TODAY_RESPONSE, actual);
+  }
+
+  @Test
+  public void taskToComplete() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(TASKS_DUE_TOMORROW);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(TASKS_DUE_TOMORROW_RESPONSE, actual);
+  }
+
+  @Test
+  public void taskCompletedYesterday() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(TASKS_COMPLETED_YESTERDAY);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(TASKS_COMPLETED_YESTERDAY_RESPONSE, actual);
+  }
+
+  @Test
+  public void taskCompletedToday() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(TASKS_COMPLETED_TODAY);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(TASKS_COMPLETED_TODAY_RESPONSE, actual);
+  }
+
+  @Test
+  public void allTasks() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(ONE_TASK_LIST);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(ALL_TASKS);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(ALL_TASKS_RESPONSE, actual);
+  }
+
+  @Test
+  public void multipleTaskLists() throws Exception {
+    Mockito.when(tasksClient.listTaskLists()).thenReturn(TWO_TASK_LISTS);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_ONE)).thenReturn(ALL_TASKS);
+    Mockito.when(tasksClient.listTasks(TASK_LIST_TWO)).thenReturn(ALL_TASKS);
+    servlet.doGet(request, response);
+    TasksResponse actual = gson.fromJson(stringWriter.toString(), TasksResponse.class);
+    Assert.assertEquals(MULTIPLE_TASK_LISTS_RESPONSE, actual);
+  }
+}
