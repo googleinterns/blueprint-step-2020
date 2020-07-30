6e576be9f5a5daec5fa451f022c88182c1fc61e6
Zachary Tan <tanzachary@google.com>
2020-07-30 11:57:31 -0400

    Debug deployed server

--- src/main/java/com/google/sps/servlets/ClientIDServlet.java
+++ src/main/java/com/google/sps/servlets/ClientIDServlet.java
@@ -25,7 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Serves the OAuth 2.0 Client ID */
 @WebServlet("/client-id")
 public class ClientIDServlet extends HttpServlet {
-  private final String clientId;
+  private String clientId;
 
   /**
    * Constructor which obtains Client ID from AuthenticationVerifier
@@ -55,6 +55,9 @@ public class ClientIDServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Cannot use JsonUtility since response needs to be a JsonObject, not just a generic object.
+    if (clientId == null) {
+      clientId = AuthenticationVerifier.getClientId();
+    }
     JsonObject clientIdJson = new JsonObject();
     clientIdJson.addProperty("client-id", clientId);
 
