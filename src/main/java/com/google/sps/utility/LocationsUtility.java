9874e6fc43422c9d0db3f9d8897fc96d52e98b2b
Zachary Tan <tanzachary@google.com>
2020-08-05 17:14:42 -0400

    Update documentation

--- src/main/java/com/google/sps/utility/LocationsUtility.java
+++ src/main/java/com/google/sps/utility/LocationsUtility.java
@@ -67,11 +67,12 @@ public final class LocationsUtility {
 
   /**
    * Generates permutations of the lists and appends them to the result parameter.
-   * 
+   *
    * @param lists A list of lists to generate permutations for.
    * @param result A pointer to a list to contain the generated permutations.
-   * @param depth An int to represent the index of the list that is currently being traversed in the recursive call.
-   * @param current A list containing the permutation built so far at the recursive call. 
+   * @param depth An int to represent the index of the list that is currently being traversed in the
+   *     recursive call.
+   * @param current A list containing the permutation built so far at the recursive call.
    */
   private static void generateCombinationsHelper(
       List<List<String>> lists, List<List<String>> result, int depth, List<String> current) {
